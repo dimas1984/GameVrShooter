beafd43621e57df4d063a88e1b7e7ceecebc5bab
dimas1984 <dimas.w@polinema.ac.id>
2020-10-05 23:30:38 +0700

    gameVr

---
+++ Assets/Script/Shoot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    // variables for the gun
+    // serializeField digunakan untuk menampilkan variabel pada jendela inspektor baik private maupun public
+    [SerializeField] new Camera camera; // reference the camera, needed for raycasting
+    [SerializeField] int damage = 1; // set default damage to enemy
+    [SerializeField] int range = 100; // range of raycast
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check input
+        if (Input.GetButtonDown("Fire1")) 
+        {
+            FireWeapon();
+        }
+    }
+
+    void FireWeapon() 
+    {
+        RaycastHit hit;
+
+        // this will check if the raycast hit anything
+        if(Physics.Raycast(camera.transform.position,camera.transform.forward,out hit, range)) 
+        {
+            print("You hit" + hit.transform.name);
+         }
+    }
+}
