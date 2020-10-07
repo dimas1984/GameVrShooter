11f98fca7a0b7f876e804971a7f30255510d3c5d
dimas1984 <dimas.w@polinema.ac.id>
2020-10-07 20:09:07 +0700

    kode nav mesh agent

---
+++ Assets/Script/enemyChase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyChase : MonoBehaviour
+{
+
+    [SerializeField] Transform theTarget;
+    [SerializeField] float forwardSpeed = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChaseTarget();
+        FaceTarget();       
+    }
+
+    void ChaseTarget() {
+        this.transform.Translate(0, 0, 0.01f * forwardSpeed);
+        //Debug.Log("Hello: " + gameObject.name);
+    }
+
+    void FaceTarget() {
+        Vector3 targetDirection = theTarget.position - this.transform.position;
+        targetDirection.y = 0;
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(targetDirection), 0.1f);
+    }
+}
---
+++ Assets/Script/playerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class playerHealth : MonoBehaviour
+{
+    [SerializeField] string levelName = "GameVr";
+    [SerializeField] int hitsToTake = 3;
+    [SerializeField] float respwanTime = 3;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Attack")
+        {
+            print("Enemy touched the player");
+            hitsToTake--;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hitsToTake <= 0) {
+            print("player has died");
+            SceneManager.LoadScene(levelName);
+        }
+        
+    }
+}
